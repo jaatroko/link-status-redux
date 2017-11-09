9984e63d1683e503db71ef4d493faf2032aabae6
Juha Aatrokoski <jha@iki.fi>
2017-11-09 16:01:43 +0200

    Insert iframe CSS also as a user-origin style sheet
    User-origin !important rules cannot be overridden even were a page
    script to change the iframe's style property. Requires Firefox 53+ to
    work, does no harm on Firefox <53 (due to try-catch). Fixes the second
    bullet point of issue #7.

---
+++ webextension/iframe.js
@@ -0,0 +1,25 @@
+// Overlay iframe id and CSS style specifications, used in content.js
+// and background.js
+
+let overlay_uuid = "e0fe7e79ef044e56993c0a0a6874baed";
+
+// Reset CSS style to initial, and then specify our modifications. All
+// done as !important, so that it cannot be overridden by document
+// CSS. (Page Javascript can still mess it up, though.) In firefox 53+
+// we insert the same CSS rules as user-origin, in which case even
+// Javascript can no longer override it.
+let overlay_iframe_css_rules =
+    "all: initial !important;" +
+    "display: block !important;" +
+    "width: 100% !important;" +
+    "height: 100% !important;" +
+    "position: fixed !important;" +
+    "left: 0 !important;" +
+    "bottom: 0 !important;" +
+    "margin: 0 !important;" +
+    "padding: 0 !important;" +
+    "pointer-events: none !important;" +
+    "z-index: 2147483647 !important;";
+
+let overlay_iframe_css = "#" + overlay_uuid
+    + " { " + overlay_iframe_css_rules + " }"; 
